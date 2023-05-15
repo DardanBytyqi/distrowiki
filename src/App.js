50ec488e57d2f54f979c893e8ed338afc8644023
DardanBytyqi <dardan76@gmail.com>
2023-05-15 19:52:10 +0200

    tolto animazione

--- src/App.js
+++ src/App.js
@@ -4,19 +4,23 @@ import Hero from './Components/Hero.js'
 import Cards from './Components/Cards.js'
 import Tabelle from './Components/Tabelle.js'
 import Footer from './Components/Footer.js'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-cards";
 
 
 function App() {
   return (
-    <div>
+   <>
         <Navbar />
         <Hero />
         <Cards />
         <Tabelle />
-        <br/>
-        <Footer />
 
-</div>
+</>
+
+
+
 
 
 
